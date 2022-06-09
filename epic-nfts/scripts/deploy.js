6254d3f71f1efd948b02889d0250dccab6f4d928
Davirain <davirain.yin@gmail.com>
2022-06-09 16:27:53 +0800

    delpoy nft contract

---
+++ epic-nfts/scripts/deploy.js
@@ -0,0 +1,30 @@
+const main = async () => {
+    const nftContractFactory = await hre.ethers.getContractFactory("MyEpicNFT");
+    const nftContract = await nftContractFactory.deploy();
+    await nftContract.deployed();
+    console.log("Contact deployed to: ", nftContract.address);
+
+    let index = 0;
+    for (index = 0; index < 5 * 5 * 5; index += 1) {
+        // Call the function 
+        let txn = await nftContract.makeAnEpicNFT();
+
+        // wait for it to be mined.
+        await txn.wait();
+        console.log("Minted NFT #%d", index);
+    }
+};
+
+
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (e) {
+        console.log(e);
+        process.exit(1);
+    }
+};
+
+runMain();
